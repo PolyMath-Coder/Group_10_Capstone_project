8cbef02c6bf0691a58bfce2319297faa59e3e6a2
Ayooluwa Adeleke <51849584+PolyMath-Coder@users.noreply.github.com>
2022-04-25 18:52:39 +0530

    Final Project Commit

--- server.js
+++ server.js
@@ -1,22 +1,46 @@
 const http = require('http');
+const path = require('path');
+const fs = require('fs');
+const os = require('os');
 
 const port = 3000;
 
+const indexPage = fs.readFileSync(__dirname + '/pages/index.html');
+const aboutPage = fs.readFileSync(__dirname + '/pages/about.html');
+const errorPage = fs.readFileSync(__dirname + '/pages/404.html');
+
 const app = http.createServer((req, res) => {
   const urlPath = req.url;
-
   if (urlPath === '/') {
-    res.statusCode = 201;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end("Hello from SideHustle...Here's the Home Page!");
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+
+    res.end(indexPage);
   } else if (urlPath === '/about') {
-    res.statusCode = 201;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end("Hello...Here's the about page");
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(aboutPage);
+  } else if (urlPath === '/sys') {
+    res.writeHead(201, { 'Content-Type': 'text/plain' });
+
+    const data = JSON.stringify({
+      hostname: os.hostname(),
+      platform: os.platform(),
+      architecture: os.arch(),
+      numberOfCPUS: os.cpus().length,
+      networkInterfaces: os.networkInterfaces(),
+      uptime: os.uptime(),
+    });
+    console.log(data);
+    res.write('Your OS info has been saved successfully!');
+    fs.writeFile('osinfo.json', data, function() {
+      console.log('The file was saved!');
+    });
+    res.end();
   } else {
     res.statusCode = 404;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Oops! You made a huge error!');
+    res.writeHead(404, { 'Content-Type': 'text/html' });
+
+    // fs.writeFileSync(path.join(__dirname, 'osinfo.json'), data);
+    res.end(errorPage);
   }
 });
 
